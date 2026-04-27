501247647d34c7dd4519bd7dcd42b908891404da
equipomaon <equipomaon@gmail.com>
2026-04-27 00:28:20 -0500

    COMMIT

--- src/routes/Ruta_uno.jsx
+++ src/routes/Ruta_uno.jsx
@@ -16,7 +16,12 @@ export const Home = () => {
       <div className="layout-pagina__seccion layout-pagina__seccion--inferior">
         <h1>home seccion 3</h1> 
       </div>
+
+      
     
+      <div className="layout-pagina__seccion layout-pagina__seccion--inferior">
+        <h1>home seccion 4</h1> 
+      </div>
     
     </> 
    
