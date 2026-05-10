a467eec54ff0d7891c03dc20cd5be1af276c47b9
equipomaon <equipomaon@gmail.com>
2026-05-10 02:39:15 -0500

    v2 -c18

--- src/routes/Ruta_Home.jsx
+++ src/routes/Ruta_Home.jsx
@@ -353,7 +353,7 @@ export const Ruta_Home = () => {
               >
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M19 12H5M5 12L12 19M5 12L12 5"/></svg>
               </button>
-              
+
               <button 
                 className="home_s6__arrow-btn" 
                 onClick={siguiente}
@@ -411,6 +411,8 @@ export const Ruta_Home = () => {
         </div>
 
       </div>
+
+      
       <div className="layout-pagina__seccion layout-pagina__seccion--media seccion_faq"></div>
 
     
