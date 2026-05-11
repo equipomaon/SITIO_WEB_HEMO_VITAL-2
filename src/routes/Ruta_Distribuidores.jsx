e2203d4fa4973e920aefcc5480b7627a72c2722a
equipomaon <equipomaon@gmail.com>
2026-05-11 13:30:52 -0500

    v3 c7

--- src/routes/Ruta_Distribuidores.jsx
+++ src/routes/Ruta_Distribuidores.jsx
@@ -1,25 +1,48 @@
+import '../styles_scss/componentes_scss/ruta_distribuidores.scss';
+
+
+//assets
+import productos_seccion_uno from '../assets/productos_seccion_uno.png'
+ 
 export const Ruta_Distribuidores = () => { 
   return (
-     <>
-    
-      <div className="layout-pagina__seccion layout-pagina__seccion--superior">
-        <h1>home seccion 1</h1>
+    <>
+      <div className="layout-pagina__seccion distribuidores_layout-pagina__seccion_uno">
+        <div className="padre_contenedor_distribuidores_seccion_uno" >
+          
+          {/* Fila superior: Texto y descripción */}
+          <div className='distribuidores_header_grid'>
+            <div className="col_izquierda">
+              <h1>Soluciones transfusionales listas para cada emergencia</h1>
+            </div>
+            
+            <div className="col_derecha">
+              <p>
+                Disponemos de sangre y hemocomponentes en distintas presentaciones, 
+                con cobertura en Colombia para responder de forma rápida y segura a 
+                cada necesidad clínica.
+              </p>
+              <div className="contenedor_boton">
+                <button className="btn-solicitar">Quiero solicitar sangre</button>
+              </div>
+            </div>
+          </div>
+
+          {/* Fila inferior: La infografía */}
+          <div className='distribuidores_seccion_uno_imagen'>
+             {/* Aquí iría la imagen del diagrama de sangre */}
+             <img src = {productos_seccion_uno} alt="Infografía Hemovital" />
+          </div>
+
+        </div>
       </div>
 
-      <div className="layout-pagina__seccion layout-pagina__seccion--media">
-         <h1>home seccion 2</h1>
+      {/* Sección dos si la necesitas después */}
+      <div className="layout-pagina__seccion distribuidores_layout-pagina__seccion_dos">
+        <div className="padre_contenedor_distribuidores_seccion_dos" >
+          {/* Contenido extra */}
+        </div>
       </div>
-
-      <div className="layout-pagina__seccion layout-pagina__seccion--inferior">
-        <h1>home seccion 3</h1> 
-      </div>
-
-      
-    
-      <div className="layout-pagina__seccion layout-pagina__seccion--inferior">
-        <h1>home seccion 4</h1> 
-      </div>
-    
     </> 
   );
 };
